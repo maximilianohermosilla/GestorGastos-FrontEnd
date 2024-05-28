dfa21c54ca531043a09abf32bda1278ec77721de
maximiliano_hermosilla <maximiliano_hermosilla@hotmail.com>
2024-05-28 01:19:51 -0300

    getCategoriasGasto, getEmpresas, getCuentas

--- GestorGastos/src/app/components/registro-gastos-page/registro-gastos-page.component.ts
+++ GestorGastos/src/app/components/registro-gastos-page/registro-gastos-page.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { CategoriaGasto } from 'src/app/models/categoria-gasto';
+import { Cuenta } from 'src/app/models/cuenta';
+import { Empresa } from 'src/app/models/empresa';
+import { CategoriaGastoService } from 'src/app/services/categoria-gasto.service';
+import { CuentaService } from 'src/app/services/cuenta.service';
+import { EmpresaService } from 'src/app/services/empresa.service';
 
 @Component({
   selector: 'app-registro-gastos-page',
@@ -7,4 +13,39 @@ import { Component } from '@angular/core';
 })
 export class RegistroGastosPageComponent {
 
+  listaCategoriaGasto: CategoriaGasto[] = [];
+  listaEmpresas: Empresa[] = [];
+  listaCuentas: Cuenta[] = [];
+
+
+  constructor(private categoriaGastoService: CategoriaGastoService, private empresaService: EmpresaService, private cuentaService: CuentaService){
+
+  }
+
+  ngOnInit(){
+    this.getCategoriaGastos();
+    this.getEmpresas();
+    this.getCuentas();
+  }
+  
+  getCategoriaGastos(){
+    this.categoriaGastoService.GetAll().subscribe((rta: CategoriaGasto[]) => {
+      this.listaCategoriaGasto = rta;
+      console.log(this.listaCategoriaGasto);
+    });
+  }
+  
+  getEmpresas(){
+    this.empresaService.GetAll().subscribe((rta: Empresa[]) => {
+      this.listaEmpresas = rta;
+      console.log(this.listaEmpresas);
+    });
+  }
+    
+  getCuentas(){
+    this.cuentaService.GetAll().subscribe((rta: Cuenta[]) => {
+      this.listaCuentas = rta;
+      console.log(this.listaCuentas);
+    });
+  }
 }
--- GestorGastos/src/app/services/cuenta.service.ts
+++ GestorGastos/src/app/services/cuenta.service.ts
@@ -1,9 +1,78 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable } from 'rxjs';
+import { DialogComponent } from '../components/shared/dialog/dialog.component';
+import { environment } from '../environment';
+import { ObjetoNombre } from '../models/objeto-nombre';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CuentaService {
+  apiUrl = environment.urlBase() + "gestorGastos/Cuenta";
+  
+  constructor(private http: HttpClient, public dialogoConfirmacion: MatDialog) { }
 
-  constructor() { }
+  public GetById(id: Number): Observable<any> {
+    return this.http.get<any>(this.apiUrl + "/" + id);
+  }
+
+  public GetAll(): Observable<any> {
+    return this.http.get<any[]>(this.apiUrl + "?idUsuario=" + 1);    
+  }
+  
+  public nuevo(element: ObjetoNombre): Observable<any> {
+    return this.http.post<ObjetoNombre>(this.apiUrl, element);
+  }
+
+  public actualizar(element: ObjetoNombre): Observable<ObjetoNombre>{
+    return this.http.put<ObjetoNombre>(this.apiUrl, element);
+  }
+
+  public eliminarById(id: number): Observable<any>{
+    return this.http.delete<any>(this.apiUrl + "/" + id);
+  }
+
+  public eliminar(id: number): Observable<any>{
+    return this.http.delete<any>(this.apiUrl + "/" + id)
+    .pipe(
+      catchError(error => {
+          let errorMsg: string;
+          if (error.error instanceof ErrorEvent) {
+              errorMsg = `Error: ${error.error}`;
+          } else {
+              errorMsg = this.getServerErrorMessage(error);
+          }
+          this.dialogoConfirmacion.open(DialogComponent, {
+            data: {
+              titulo: "Error",
+              mensaje: errorMsg,
+              icono: "warning",
+              clase: "class-error"
+            }
+          })
+          return errorMsg;
+      })
+    );
+  }
+
+  private getServerErrorMessage(error: HttpErrorResponse): string {    
+    switch (error.status) {
+        case 404: {
+            return `Not Found: ${error.error}`;
+        }
+        case 403: {
+            return `Access Denied: ${error.message}`;
+        }
+        case 500: {
+            return `Internal Server Error: ${error.message}`;
+        }
+        default: {
+            //return `Unknown Server Error: ${error.error}`;
+            return `${error.error}`;
+        }
+
+    }
+  }
 }
